171ac69bdbd2528da538db89c32f385e074d5796
MADANI MOHAMED EL-HABIB <90145613+madanimohadhabib@users.noreply.github.com>
2023-10-25 02:27:09 +0200

    cupagis

--- setup/settings.py
+++ setup/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cupagis', 
+        'USER': 'cupagisdb',
+        'PASSWORD': 'XSJKZ7Pt18OZIMN4VEhJbFThFPMEH9xH',
+        'HOST': 'dpg-cks5vjfd47qs73arpvo0-a', 
+        'PORT': '5432',
     }
 }
 
@@ -117,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Répertoire pour les fichiers statiques de l'application
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Répertoire pour les fichiers statiques collectés en production
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static') # Répertoire pour les fichiers statiques de l'application
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') # Répertoire pour les fichiers statiques collectés en production
 
 # Paramètres de fichiers média
 MEDIA_URL = '/media/'
