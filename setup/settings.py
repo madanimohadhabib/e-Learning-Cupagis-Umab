53283f5e1b3089a75b1f8032e5ff95d05faa38ec
MADANI MOHAMED EL-HABIB <90145613+madanimohadhabib@users.noreply.github.com>
2023-10-25 02:32:12 +0200

    cupagis

--- setup/settings.py
+++ setup/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static') # Répertoire pour les fichiers statiques de l'application
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') # Répertoire pour les fichiers statiques collectés en production
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Répertoire pour les fichiers statiques de l'application
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Répertoire pour les fichiers statiques collectés en production
 
 # Paramètres de fichiers média
 MEDIA_URL = '/media/'
